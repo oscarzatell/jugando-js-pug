7e0411ab8df94758afca2d89f4ff3bb751cd1ea5
oscarzamora <oscarnewsletter1@gmail.com>
2020-06-20 00:01:49 -0500

    iniciando con router.js

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const Router = require("./router/router");
 
 app.set("view engine", "pug");
-
+/*
 app.get("/", (req, res) => {
   res.render("inicio");
 });
+*/
+app.use("/", Router);
 
 app.listen(3000, () => console.log("App lista!"));
