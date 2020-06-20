e0ea8fa4cdac645d8731f704627921fb23e15786
oscarzamora <oscarnewsletter1@gmail.com>
2020-06-20 01:25:24 -0500

    iterar variables y arrays en el archivo pug

--- router/router.js
+++ router/router.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+let hola = "hola mundo este es un texto desde una variable externa";
+let colores = ["rojo", "azul", "amarillo"];
+
 router.get("/", (req, res) => {
-  res.render("inicio");
+  res.render("inicio", { hola, colores });
 });
 
 module.exports = router;
